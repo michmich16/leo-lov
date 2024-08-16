25da475d3f743cb30e643281e3427adf2a684773
Michael P <102651473+michmich16@users.noreply.github.com>
2024-08-16 13:21:48 +0200

    indtil videre

--- leo-lov-project/src/App.jsx
+++ leo-lov-project/src/App.jsx
@@ -44,7 +44,10 @@ function App() {
           })}
         </div>
       </div>
-      <Article img='intetproblem.jpeg' articleHeader='NTET PROBLEM' articleText='Leo-lov har altid været det bedste advokatfirma i verden. Der er simpelthen ingen der kan måle sig med os. Vi kan løse alle problemer så længe du har penge. Hvis du mod alt forventning skulle oplevel at støde på et retsligt problem som leo-lov ikke kan løse får du halvdelen af beløbet igen. <br> Det er sådan vi har skabt en forretning der ikke kan andet end at vokse!'/>
+      <div className='sectionFlex'>
+        <Article img='intetproblem.jpeg' articleHeader='NTET PROBLEM' articleText='Leo-lov har altid været det bedste advokatfirma i verden. Der er simpelthen ingen der kan måle sig med os. Vi kan løse alle problemer så længe du har penge. Hvis du mod alt forventning skulle oplevel at støde på et retsligt problem som leo-lov ikke kan løse får du halvdelen af beløbet igen.' subarticleText='Det er sådan vi har skabt en forretning der ikke kan andet end at vokse!' />
+        <Article  articleHeader='NTET PROBLEM' articleText='Leo-lov har altid været det bedste advokatfirma i verden. Der er simpelthen ingen der kan måle sig med os. Vi kan løse alle problemer så længe du har penge. Hvis du mod alt forventning skulle oplevel at støde på et retsligt problem som leo-lov ikke kan løse får du halvdelen af beløbet igen.' subarticleText='Det er sådan vi har skabt en forretning der ikke kan andet end at vokse!' img='intetproblem.jpeg'/>
+      </div>
     </>
   )
 }
--- leo-lov-project/src/components/Article/Article.jsx
+++ leo-lov-project/src/components/Article/Article.jsx
@@ -2,11 +2,12 @@ import style from './Article.module.scss'
 
 export const Article = (props) => {
     return (
-        <article>
-            <img src={`./assets/images/${props.img}`} />
-            <div>
+        <article className={style.infoBoxStyle}>
+            <img src={`./${props.img}`} />
+            <div className={style.infoTextStyle}>
                 <h3>{props.articleHeader}</h3>
                 <p>{props.articleText}</p>
+                <p>{props.subarticleText}</p>
             </div>
         </article>
     )
